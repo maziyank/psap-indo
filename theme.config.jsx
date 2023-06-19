e3a5db26d38b4797c7903d8f59ea6b31de3c3a10
Bakhtiar Amaludin <bakhtiar.amaludin@kemenkeu.go.idn>
2023-06-19 08:37:02 +0700

    update

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,5 +2,6 @@ export default {
     logo: <span>SAP Indonesia</span>,
     project: {
       link: 'https://github.com/maziyank/psap-indo'
-    } 
+    },
+    docsRepositoryBase: 'https://github.com/maziyank/psap-indo/blob/main/docs/pages'
   }
